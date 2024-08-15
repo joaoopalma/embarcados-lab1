261b9fb250c755a4ec403be74947e4d24fb24273
joaoopalma <103431544+joaoopalma@users.noreply.github.com>
2024-08-15 11:15:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,16 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5;
+    int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
     }
 
